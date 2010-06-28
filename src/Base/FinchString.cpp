10387de0aae3b5ee683aca7bc18d454d4b841afa
Zac Brown <zac@zacbrown.org>
2010-06-27 21:35:02 -0700

    Change from String& to const char* in String::Format due to strange Windows formatting in console. Text displayed as non-intelligible UNICODE instead of appropriate text. Tested on Mac OSX as well.

--- src/Base/FinchString.cpp
+++ src/Base/FinchString.cpp
@@ -8,14 +8,14 @@ namespace Finch
 {
     const char * String::sEmptyString = "";
     
-    String String::Format(const String & format, ...)
+    String String::Format(const char* format, ...)
     {
         char result[FormattedStringMax];
         
         va_list args;
         va_start (args, format);
         
-        vsprintf(result, format.CString(), args);
+        vsprintf(result, format, args);
 
         va_end (args);
         
