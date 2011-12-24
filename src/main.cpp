87648c7de127264971b41dcabe072292d54c81ad
Bob Nystrom <robert@stuffwithstuff.com>
2011-12-23 21:03:17 -0800

    Fix loading core lib from correct path.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <stdlib.h> // realpath
+#include <sys/param.h> // PATH_MAX
 
 #include "FileLineReader.h"
 #include "FinchString.h"
@@ -50,43 +52,48 @@ PRIMITIVE(LoadFile)
 }
 
 int main (int argc, char * const argv[])
-{
+{    
     StandaloneInterpreterHost host;
     Interpreter               interpreter(host);
     
-    // set up the standalone-provided behavior
+    // Set up the standalone-provided behavior.
     interpreter.BindMethod("Ether", "load:", LoadFile);
     
-    // load the base library
-    //### bob: hard-coded path here is a total hack
-#ifdef HACK_ROOT_BASE_PATH
-    const char* baseLibPath = "lib/core.fin";
-#else
-    const char* baseLibPath = "../../lib/core.fin";
-#endif
-    if (!InterpretFile(interpreter, baseLibPath))
+    // Figure out the absolute path to the core library, relative to the
+    // executable. Assumes a directory layout like:
+    // finch
+    // - build/<config>/finch
+    // - lib/core.fin
+    char coreLibPath[PATH_MAX];
+    char fullPath[PATH_MAX];
+    strncpy(coreLibPath, argv[0], PATH_MAX);
+    strncat(coreLibPath, "/../../../lib/core.fin", PATH_MAX);
+    realpath(coreLibPath, fullPath);
+    
+    // Load the core library.
+    if (!InterpretFile(interpreter, fullPath))
     {
-        cout << "Could not load base library." << endl;
+        cout << "Could not load core library." << endl;
         return 2;
     }
     
     if (argc == 1)
     {
-        // with no arguments (arg zero is app), run in interactive mode
+        // With no arguments (arg zero is app), run in interactive mode.
         cout << "Finch 0.0.0d" << endl;
         cout << "------------" << endl;
         
         //### bob: host should provide some external function to set a "quit"
-        // flag so we can exit this loop.
+        // Flag so we can exit this loop.
         while (true) {
-            // ansi color: std::cout << "\033[0;32m";
+            // ANSI color: std::cout << "\033[0;32m";
             ReplLineReader reader;
             interpreter.Interpret(reader, true);
         }
     }
     else if (argc == 2)
     {
-        // one argument, load and execute the given script
+        // One argument, load and execute the given script.
         String fileName = argv[1];
         return InterpretFile(interpreter, fileName) ? 0 : 1;
     }
